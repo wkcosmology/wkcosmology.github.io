805efc416f073b7374e9261e13029d92c5bf3933
Kosmos <wkcosmology@gmail.com>
2021-03-30 19:49:22 -0400

    test dns

--- js/md.js
+++ js/md.js
@@ -1,5 +1,5 @@
 var blog = document.querySelector("#blog1");
-var text = "# hello, markdown!";
+var text = "# hello, markdown!ss";
 blog.innerHTML += text;
 var converter = new showdown.Converter();
 blog.innerHTML += converter.makeHtml(text);
